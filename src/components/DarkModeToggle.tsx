792ed00c2f40a7eb37658853e349db41c08993d4
Michael Schlueter <schluet0r4life@gmail.com>
2024-03-12 17:36:01 +0100

    add styling for mobile header

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -3,11 +3,11 @@ import * as Switch from "@radix-ui/react-switch";
 export default function DarkModeToggle() {
   return (
     <div className="switch-container">
-      <img src="/assets/images/icon-sun-dark.svg" alt=""></img>
+      <img className="switch-icon" src="/assets/images/icon-sun-dark.svg" alt=""></img>
       <Switch.Root className="switch-root" id="darkmode">
         <Switch.Thumb className="switch-thumb" />
       </Switch.Root>
-      <img src="/assets/images/icon-moon-dark.svg" alt=""></img>
+      <img className="switch-icon" src="/assets/images/icon-moon-dark.svg" alt=""></img>
     </div>
   );
 }
